6627c40014f37265f07754f5ed02d32184edb42c
riteshkumar81 <riteshkumarmohanta6@gmail.com>
2026-09-19 11:27:46 +0530

    Fix Task model import case

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Task = require("../models/task");
+const Task = require("../models/T ask");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
